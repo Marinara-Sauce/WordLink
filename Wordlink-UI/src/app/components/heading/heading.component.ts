c961e64a980197a51783bc466875c34c3d5ee570
Dan Bliss <danbliss14027@gmail.com>
2023-02-26 09:37:31 -0500

    Generated help function

--- Wordlink-UI/src/app/components/heading/heading.component.ts
+++ Wordlink-UI/src/app/components/heading/heading.component.ts
@@ -10,12 +10,22 @@ export class HeadingComponent implements OnInit {
 
   constructor(private puzzleService: PuzzleServiceService) { }
 
+  helpVisible: boolean = false;
+
   ngOnInit(): void {
   }
 
   generateNewPuzzle(): void {
     this.puzzleService.generatePuzzle().subscribe(() =>
       window.location.reload());
+  }
+
+  showHelp(): void {
+    this.helpVisible = true;
+  }
+
+  hideHelp = (): void => {
+    this.helpVisible = false;
   }
 
 }
