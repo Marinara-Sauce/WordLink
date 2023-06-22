fbd06016eb199c5b1d0db74231d82ca221ccb130
Dan Bliss <danbliss14027@gmail.com>
2023-06-22 15:56:56 -0400

    Began working on docker compose

--- Wordlink-API/spawn-db.sql
+++ Wordlink-API/spawn-db.sql
@@ -1,4 +1,4 @@
-create table if not exists puzzles
+create table if not exists puzzle
 (
     p_id     int auto_increment
     primary key,
--- Wordlink-API/src/main/java/com/marinara/wordlink/PersistenceContext.java
+++ Wordlink-API/src/main/java/com/marinara/wordlink/PersistenceContext.java
@@ -3,17 +3,9 @@ package com.marinara.wordlink;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
-import org.jooq.impl.DefaultConfiguration;
-import org.jooq.impl.DefaultDSLContext;
-import org.jooq.impl.DefaultExecuteListenerProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
-import javax.sql.DataSource;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
