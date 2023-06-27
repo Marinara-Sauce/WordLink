e4d673ca952b780df32e7d9dc6b40243f9d8aafd
Dan Bliss <danbliss14027@gmail.com>
2023-06-27 12:18:14 -0400

    build scripts

--- Wordlink-UI/src/environments/environment.prod.ts
+++ Wordlink-UI/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true
 };
+
+export const httpUrl = 'http://34.148.135.15:8080';
---
+++ buildkite/scripts/launch_db.sh
@@ -0,0 +1,1 @@
+docker-compose -f ../../Wordlink-API/docker-compose.yml -d
